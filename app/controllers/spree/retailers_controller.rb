cddca9aa954904c3554bc6c6ec9b79ab09975e1d
Michael Bianco <iloveitaly@gmail.com>
2014-04-01 09:30:27 -0400

    Cleaning up user facing retailer controller

--- app/controllers/spree/retailers_controller.rb
+++ app/controllers/spree/retailers_controller.rb
@@ -1,27 +1,22 @@
 class Spree::RetailersController < Spree::BaseController
 
   def index
-    @retailer_types = Spree::RetailerType.order(:name).all
-    @retailers = default_scope.all
-    get_states
-  end
-
-private 
-
-  def default_scope
-    return @default_scope if @default_scope
-    scope = if params[:retailer_type_id].present?
+    # TODO right now the index doesn't categorize the different type of retailers well
+    # it's really messy; don't have time to fix it right now
+    
+    if params[:retailer_type_id].present?
       @retailer_type = Spree::RetailerType.find_by_permalink!(params[:retailer_type_id])
-      @retailer_type.retailers
+      @retailers = @retailer_type.retailers
     else
-      Spree::Retailer.scoped
+      @retailers = Spree::Retailer.scoped
     end
-    @default_scope = scope.order(:country, :state, :name)
-  end
 
-  def get_states
+    @retailer_types = Spree::RetailerType.order(:name).all
+    @retailers = @retailers.order(:country, :state, :name)
+
+    # TODO this whole state selection thing is a mess; needs to be cleaned up
     @states = {}
-    Spree::State.where(:abbr => @retailers.collect(&:state).uniq).all.each do |state|
+    Spree::State.where(abbr: @retailers.map(&:state).uniq.reject(&:blank?)).all.each do |state|
       @states[state.abbr] = state.name
     end
   end
