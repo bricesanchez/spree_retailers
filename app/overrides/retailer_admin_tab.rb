f775a057960dd5cb9d6723a8b0df121c45163385
David Crees <davidc@Minke.local>
2014-05-30 22:45:46 +0100

    Moved retailer type from configuration to the retailer submenu

--- app/overrides/retailer_admin_tab.rb
+++ app/overrides/retailer_admin_tab.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path  => "spree/layouts/admin",
                      :name          => "add_retailers_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text          => "<%= tab(:retailers, :local_stockists) %>",
+                     :text          => "<%= tab(:retailers, :retailer_types, :local_stockists) %>",
                      :disabled      => false,
                      :original      => '031652cf5a054796022506622082ab6d2693699f')
