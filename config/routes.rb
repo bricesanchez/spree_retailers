cf6744d16a4583817efe740d37e30d8b527aee91
Brice Sanchez <contact@brice-sanchez.com>
2014-10-17 01:56:10 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Spree::Core::Engine.routes.draw do
   namespace :admin do
     resources :retailer_types, :constraints => { :id => /.*/ }
     resources :retailers
-    match '/find-local-stockists' => 'retailers#find_local_stockists', :as => :find_local_stockists
+    get '/find-local-stockists' => 'retailers#find_local_stockists', :as => :find_local_stockists
   end
 
   get '/retailers(/:retailer_type_id)' => 'retailers#index', :as => :retailer_type
