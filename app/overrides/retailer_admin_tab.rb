32a273f7e7de1864a83c64888d31667ee7fc5861
Brice Sanchez <contact@brice-sanchez.com>
2014-10-17 01:57:34 -0400

    Add retailer link in admin main menu

--- app/overrides/retailer_admin_tab.rb
+++ app/overrides/retailer_admin_tab.rb
@@ -1,3 +1,11 @@
+Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
+                     :name => "admin_retailers_tab",
+                     :insert_bottom => "[data-hook='admin_tabs']",
+                     :text => "<% if can? :admin, Spree::Admin::RetailersController %>
+                                 <%= tab :retailers,  :url => spree.admin_retailers_path, :icon => 'globe' -%>
+                               <% end %>",
+                     :disabled => false)
+
 Deface::Override.new(:virtual_path  => "spree/layouts/admin",
                      :name          => "add_retailers_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
